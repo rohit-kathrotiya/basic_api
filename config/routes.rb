e79f55dae2b65ba6586a327c0901ce2b0b119ec0
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-06-09 21:12:50 +0200

    Using of namespace for different versions of api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :bands
-  resources :members
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :bands do
+        resources :members
+      end
+    end
+    
+    namespace :v2 do
+      resources :bands do
+        resources :members
+      end
+    end
+  end
 end
